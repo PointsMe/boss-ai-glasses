22ead8be6c3d083754690ce39e47d2e96c72f7fc
Msoepa0528 <hongchangliang8@gmail.com>
2025-07-08 18:03:42 +0800

    fix

--- src/views/store/storeList/form.vue
+++ src/views/store/storeList/form.vue
@@ -138,7 +138,7 @@ defineExpose({ getRef });
           </el-select>
         </el-form-item>
       </el-col>
-      <el-col :span="24">
+      <!-- <el-col :span="24">
         <el-form-item label="编号" prop="code">
           <el-input
             v-model="newFormInline.code"
@@ -146,7 +146,7 @@ defineExpose({ getRef });
             placeholder="请输入编号"
           />
         </el-form-item>
-      </el-col>
+      </el-col> -->
       <el-col :span="24">
         <el-form-item label="名称" prop="name">
           <el-input
